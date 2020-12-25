a08352dbc0a8becd6945fdb0ff9960c257ed4a9c
Gireesh Bogu <gireeshkbogu@users.noreply.github.com>
2020-12-25 11:14:49 -0800

    Update laad_covid19.py

--- laad_covid19.py
+++ laad_covid19.py
@@ -425,11 +425,11 @@ class LAAD:
         max = stats.filter(like='max', axis=0)
         max = float(max[0])
         
-        #THRESHOLD = max
+        
         # We can calculate the mean and standard deviation of training data loss 
         # then calculate the cut-off as more than 2 standard deviations from the mean.
         # We can then identify anomalies as those examples that fall outside of the defined upper limit.
-        cut_off = std * 3
+        #cut_off = std * 3
         #THRESHOLD =  mean + cut_off
         THRESHOLD =  max
         return THRESHOLD
