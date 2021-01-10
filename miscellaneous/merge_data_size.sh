766d878d9e149d80549195f0bef6c483260a2252
Gireesh Bogu <gireeshkbogu@users.noreply.github.com>
2021-01-10 06:34:43 -0800

    Delete merge_data_size.sh

--- miscellaneous/merge_data_size.sh
+++
@@ -1,5 +0,0 @@
-cat results/1_covid_positive/*_data_split_dates.csv |grep -v end |awk '{print $1",""COVID-19,"$2","$4","$6","$8","$10","$12}' > size.csv
-cat results/2_non_covid/*_data_split_dates.csv |grep -v end |awk '{print $1",""Non-COVID-19,"$2","$4","$6","$8","$10","$12}' >> size.csv
-cat results/3_healthy/*_data_split_dates.csv |grep -v end |awk '{print $1",""Healthy,"$2","$4","$6","$8","$10","$12}' >> size.csv
-
-
